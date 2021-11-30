2d77b7002b777816f3f566601a937c646a9aaa26
Artem <lvoicel@ya.ru>
2021-11-30 16:53:05 +0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -15,26 +15,26 @@ You can refer to the website for the API documentation and examples.
 
 testCase('Test GET /posts?userId=<id>&title=<title>', () => {
 	// test for GET	/posts by valid 'userId' and 'title'
-	it('it should GET post userId and title', (done) => {
-		const userId = 1;
-		const title = 'qui est esse';
-		chai.request('https://jsonplaceholder.typicode.com')
-			.get('/posts?userId=' + userId + '&title=' + title)
-			.end((err, res) => {
-				res.should.have.status(200);
-				res.body[0].userId.should.be.a('number');
-				res.body[0].id.should.be.a('number');
-				res.body[0].title.should.be.a('string');
-				res.body[0].body.should.be.a('string');
-				res.body.should.to.be.a('array').to.have.length(1)
-				done();
-			});
-	});
+    it('it should GET post userId and title', (done) => {
+	const userId = 1;
+	const title = 'qui est esse';
+	chai.request('https://jsonplaceholder.typicode.com')
+	    .get('/posts?userId=' + userId + '&title=' + title)
+	    .end((err, res) => {
+	        res.should.have.status(200);
+	        res.body[0].userId.should.be.a('number');
+	        res.body[0].id.should.be.a('number');
+	        res.body[0].title.should.be.a('string');
+	        res.body[0].body.should.be.a('string');
+	        res.body.should.to.be.a('array').to.have.length(1)
+	        done();
+       	    });
+    });
 
 	// test for GET	/posts with wrong 'userId' and 'title'
     it('it should GET empty array by entering wrong userId and title', (done) => {
-		const userId = '101';
-		const title = '11wrong title11';
+	const userId = '101';
+	const title = '11wrong title11';
         chai.request('https://jsonplaceholder.typicode.com')
             .get('/posts?userId=' + userId + '&title=' + title)
             .end((err, res) => {
@@ -59,8 +59,8 @@ testCase('Test GET /posts?userId=<id>&title=<title>', () => {
 	
 	// test for GET	/posts with valid 'userId' and wrong 'title'
     it('it should GET empty array by entering wrong userId and valid title', (done) => {
-		const userId = '1';
-		const title = '11wrong title11';
+        const userId = '1';
+        const title = '11wrong title11';
         chai.request('https://jsonplaceholder.typicode.com')
             .get('/posts?userId=' + userId + '&title=' + title)
             .end((err, res) => {
@@ -70,19 +70,30 @@ testCase('Test GET /posts?userId=<id>&title=<title>', () => {
             });
     });
 	
-	// test for GET	/posts with only with 'userId'
+	// test for GET	/posts only with 'userId'
     it('it should GET array with some posts by userId', (done) => {
-		const userId = '1';
+        const userId = '1';
         chai.request('https://jsonplaceholder.typicode.com')
             .get('/posts?userId=' + userId)
             .end((err, res) => {
                 res.should.have.status(200);
-				res.body[0].should.have.property('userId');
-				res.body[0].should.have.property('body');
-				res.body[0].should.have.property('id');
-				res.body[0].should.have.property('title');
-				res.body.should.to.be.a('array').is.not.empty;
+	        res.body[0].should.have.property('userId');
+	        res.body[0].should.have.property('body');
+	        res.body[0].should.have.property('id');
+	        res.body[0].should.have.property('title');
+	        res.body.should.to.be.a('array').is.not.empty;
                 done();
             });
+    });
+	
+	// test for GET	/posts without 'userId' ant 'title'
+    it('it should GET array with some posts by userId', (done) => {
+        chai.request('https://jsonplaceholder.typicode.com')
+            .get('/posts?userId=''&title='')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.to.be.empty;
+                done();
+	    });
     });
 });
